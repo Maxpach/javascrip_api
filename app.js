d5835251e423c6d8e71d5e87ee4f00fde6c52e26
Leandro Maximiliano <32625973+Maxpach@users.noreply.github.com>
2020-05-14 10:16:54 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// get - post - delete - update 
+const route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "Node Store API",
+        version: "0.0.2"
+    });
+});
+
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+app.use('/', route);
+app.use('/products', create);
+
+module.exports = app;
